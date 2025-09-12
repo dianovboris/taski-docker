b8745e3b8e2fe69746e6d6154a74cd57b93243b4
Boris Dianov <dianov.boris@gmail.com>
2025-09-12 16:30:45 +0300

    PEP8

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,8 +1,8 @@
 import os
+from django.core.management.utils import get_random_secret_key
 from pathlib import Path
 
 from dotenv import load_dotenv
-from django.core.management.utils import get_random_secret_key
 
 load_dotenv()
 
